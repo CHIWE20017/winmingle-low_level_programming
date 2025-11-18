3d344222f1ded0bb7f44800b3b9ae6dcd190ae26
chiwegit <ajaoguchiwendutessy@gmai.com>
2025-11-18 04:28:41 +0100

    Add C program that prints quote to stderr using write

--- 101-quote.c
+++ 101-quote.c
@@ -1,15 +1,14 @@
+#include <unistd.h>
 /**
- * Author:Ajaogu Chiwendu Tessy
- * Program: Winmingle Community C Training
- * Description: Print a qoute to standard error using write()
- */
-
-#include<unistd.h>
-
+* Author: Ajaogu Chiwendu Tessy
+* Program: WinMingle Community C Training
+*Description: Writing a C program with putchar
+*/
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return(1);
 
 }
