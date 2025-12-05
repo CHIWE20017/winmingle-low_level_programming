179729e9a44875b93cbd134a29b9a0bab3549065
chiwegit <ajaoguchiwendutessy@gmai.com>
2025-12-05 05:35:53 +0100

    Write in lowercase in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/**
+ * Authur: Ajaogu Chiwendu Tessy
+ * Program: Winmingle Community C Training
+ * Description: Write z - a
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char x = 'z';
+
+	while(x > 'a'){
+		putchar(x);
+		x--;
+	}
+
+	putchar('\n');
+
+        return (0);
+}
