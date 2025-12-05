e083e2ebcd00cd4041b672ca88d8556c7290054f
chiwegit <ajaoguchiwendutessy@gmai.com>
2025-12-05 04:35:06 +0100

    Write alphabet in both lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,33 @@
+/**
+ * Authur: Ajaogu Chiwendu Tessy
+ * Program: Winmingle Communuty C Training
+ * Description
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char x = 'a';
+	char X = 'A';
+        
+        while (x <= 'z'){
+	putchar (x);
+	x++;
+	}
+	while (X <= 'Z'){
+		putchar (X);
+		X++;
+	}
+
+	putchar('\n');
+
+	 
+
+	
+	return (0);
+
+
+
+}
