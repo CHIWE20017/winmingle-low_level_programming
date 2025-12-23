9cb7a1455965dad0d6b7e0513006279756feba0d
chiwegit <ajaoguchiwendutessy@gmai.com>
2025-12-23 06:24:45 +0100

    writing a program that print a lower case of an alphabet 10 times

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * Authur: Ajaogu Chiwendu Tessy
+ * Program: Winmingle Community C Training
+ * Descreption: Defining _putchar function
+ */
+
+int add(int i, int j) 
+{
+	return (i + j);
+}
