62dab74c343dd590a800bc9a20d55bd6fe0978a3
chiwegit <ajaoguchiwendutessy@gmai.com>
2025-12-23 06:23:03 +0100

    writing a program that print a lower case of an alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Authur: Ajaogu Chiwendu Tessy
+ * Program: Winmingle Community C Training
+ * Descreption: Writing lowercase alphabet
+ */
+
+ void print_alphabet(void) 
+{
+	char x;
+
+	for(x = 'a'; x <= 'z'; x++) 
+	{
+		_putchar(x);
+	
+	}
+	_putchar('\n');
+ 
+ }
