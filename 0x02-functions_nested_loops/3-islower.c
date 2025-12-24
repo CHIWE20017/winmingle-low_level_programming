e7946ff5282fc1e9a2516bf4bd7921261167654f
chiwegit <ajaoguchiwendutessy@gmai.com>
2025-12-24 03:50:22 +0100

    learning more about man is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * Authur: Ajaogu Chiwendu Tessy
+ * Program: Winmingle Community C training Program
+ * Description: Writing lowercase character
+ */
+
+int _islower(int c) 
+{
+	if(c >= 'a' && c <= 'z') 
+	{
+		return 1;
+	
+	}
+
+	return 0;
+}
