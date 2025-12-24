5c8b1f339bc138851dd0bb64cf905a091a8cbad1
chiwegit <ajaoguchiwendutessy@gmai.com>
2025-12-24 04:02:35 +0100

    writimg codr that prints absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Authur: Ajaogu Chiwendu Tessy
+ * Progeram: Winmingle Community C Training
+ * Description: Writing a function that _putchar that absolut value of an integer
+ */
+
+int _abs(int r) 
+{
+	if (r < 0) {
+	
+		return(-r);
+
+       }
+	
+	return(r);
+
+}
