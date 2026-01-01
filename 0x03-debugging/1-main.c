c9eb6a594c6f69b7e0d9f402030d08f0ef42dba1
chiwegit <ajaoguchiwendutessy@gmai.com>
2026-01-01 16:16:55 +0100

    Stop an infinite loop by commenting out the problematic line.

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+/**
+* main - causes an infinite loop
+* Return: 0
+*
+* Authur: Ajaogu Chiwendu Tessy
+* Program: Winmingle community c training
+* Description: Learning out how to debug/ comment code that will cause an infinit loop
+*/
+int main(void)
+{
+    int i;
+ 
+    /**printf("Infinite loop incoming :(\n");
+ 
+   * i = 0;
+ 
+    *while (i < 10)
+    *{
+     *   putchar(i);
+    *}
+ */
+    printf("Infinite loop avoided! \\o/\n");
+ 
+    return (0);
+}
+
